8d8a44cef49aeb572e28cce8654b9274b45ac47d
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-12-22 00:21:37 +0000

    project done

--- src/app.py
+++ src/app.py
@@ -13,9 +13,21 @@ def get_todos():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data
-    print('Incoming request with the following body', request_body) 
-    return 'Response for the POST todo'
+    request_body = request.get_json(force=True)
+
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print('This is the position to delete: ', position)
+
+    if 0 <= position < len(todos):
+        deleted_task = todos.pop(position)
+        print('Deleted task:', deleted_task)
+        return jsonify(todos)
+    else:
+        return jsonify({'error': 'Invalid position'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
